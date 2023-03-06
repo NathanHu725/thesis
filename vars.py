127c66e1e9561eff106234c03a08403c20bf2418
Nathan Hu <nahu725@gmail.com>
2023-03-06 10:37:48 -0500

    small changes

--- vars.py
+++ vars.py
@@ -49,10 +49,11 @@ class VarGetter:
         self.cities_extended = [('Milwaukee', 570000), ('Rockford', 147000), ('Gary', 70000), ('Chicago', 2670000), ('Minneapolis', 425000), ('St. Paul', 307000), ('Madison', 270000), ('Indianapolis', 882000), ('Fort Wayne', 265974), ('Des Moines', 212031), ('Aurora', 179266), ('Grand Rapids', 197416), ('Overland Park', 197106), ('Akron', 189347), ('Sioux Falls', 196528), ('Springfield MO', 169724), ('Kansas City MO', 508394), ('Joliet', 150371), ('Naperville', 149104), ('Dayton', 137571), ('Warren', 138130), ('Olathe', 143014), ('Sterling Heights', 131996), ('Cedar Rapids', 130330), ('Topeka', 127139), ('Fargo', 125804), ('Rochester', 124599), ('Evansville', 119806), ('Ann Arbor', 119303), ('Columbia', 118620), ('Independence', 117369), ('Springfield IL', 116313), ('Peoria', 115424), ('Lansing', 115222), ('Elgin', 112628), ('Green Bay', 104796), ('Toledo', 268508), ('Lincoln', 293000), ('St. Louis', 301000), ('Columbus', 905000), ('Detroit', 639000), ('Kansas City KS', 477000), ('Omaha', 463000), ('Wichita', 397000), ('Cleveland', 373000), ('Cincinnati', 309000)]
         self.cities = [('Milwaukee', 570000), ('Rockford', 147000), ('Gary', 70000), ('Chicago', 2670000), ('Minneapolis', 425000), ('St. Paul', 307000), ('Madison', 270000), ('Indianapolis', 882000)]
         self.cities_one = [('Chicago', 2670000)]
+        self.cities_small = [('Chicago', 2670000), ('Milwaukee', 570000), ('Rockford', 147000), ('Gary', 70000)]
 
         self.distances_one = [[0]]
 
-        self.distances_inf = [[0, 100, float('inf'), float('inf')], 
+        self.distances_small = [[0, 100, float('inf'), float('inf')], 
                     [100, 0, float('inf'), 50], 
                     [float('inf'), float('inf'), 0, 30],
                     [float('inf'), 50, 30, 0]]
